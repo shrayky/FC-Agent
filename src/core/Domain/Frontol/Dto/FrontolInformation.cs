f2b49457e19963f2fa378ca1140a15fbd1d33d87
arseniiz <shrayky@au124.ru>
2025-11-19 12:28:30 +0700

    рефакторинг

--- src/core/Domain/Frontol/Dto/FrontolInformation.cs
+++ src/core/Domain/Frontol/Dto/FrontolInformation.cs
@@ -3,5 +3,5 @@ namespace Domain.Frontol.Dto;
 public class FrontolInformation
 {
     public string Version { get; set; } = string.Empty;
-    public Frontol.Dto.FrontolSettings Settings { get; set; } = new();
+    public FrontolSettings Settings { get; set; } = new();
 }
