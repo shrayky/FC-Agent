5260dd717d56e073986ba003ca1ecc824ea0530d
arseniiz <shrayky@au124.ru>
2025-11-20 19:15:59 +0700

    доавлен тип сообщения

--- src/core/Domain/Messages/Dto/AgentStateResponse.cs
+++ src/core/Domain/Messages/Dto/AgentStateResponse.cs
@@ -1,11 +1,14 @@
 using Domain.Agent.Dto;
 using Domain.Frontol.Dto;
+using Domain.Messages.Enums;
+using Domain.Messages.Interfaces;
 
 namespace Domain.Messages.Dto;
 
-public record Message
+public record AgentStateResponse : IMessage
 {
     public string AgentToken { get; set; } = string.Empty;
+    public MessageType MessageType { get; set; } = MessageType.AgentState;
     public AgentData AgentInformation { get; set; } = new();
     public FrontolInformation FrontolInformation { get; set; } = new();
     public List<LicenseInformation> Licenses { get; set; } = [];
