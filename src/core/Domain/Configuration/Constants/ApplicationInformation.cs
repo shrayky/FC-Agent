111b7510189356a7d96025b0c5ef9b1decd6ca10
Арсений Жарков <shrayky@au124.ru>
2026-09-04 16:05:35 +0700

    new: версия 2.7

--- src/core/Domain/Configuration/Constants/ApplicationInformation.cs
+++ src/core/Domain/Configuration/Constants/ApplicationInformation.cs
@@ -8,7 +8,7 @@
         public const string ServiceName = "DS:FC Agent";
         
         public const int Version = 2;
-        public const int Assembly = 6;
+        public const int Assembly = 7;
 
         public static object Information() => new { Name, Version, Assembly, Description};
     }
